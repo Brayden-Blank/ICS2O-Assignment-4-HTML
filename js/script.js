929ef98215a421ac9bee3a7322f3e8b798fc131c
Brayden Blank <99354754+Brayden-Blank@users.noreply.github.com>
2022-05-02 17:11:29 +0000

    made some changes

--- js/script.js
+++ js/script.js
@@ -7,7 +7,7 @@
 "use strict";
 
 /**
- * Check servie worker.
+ * Check service worker.
  */
 if (navigator.serviceWorker) {
   navigator.serviceWorker.register("/ICS2O-Unit-5-02-HTML/sw.js", {
@@ -15,20 +15,50 @@ if (navigator.serviceWorker) {
   });
 }
 
-/**
- * This function compares slider with random number.
- */
-function determineClicked() {
-  const chosenNumber = parseInt(document.getElementById("chosen").value);
+function orderClicked() {
+  var largePizza = document.getElementById("large");
+  var extraLargePizza = document.getElementById("extra-large");
+  var oneTopping = document.getElementById("one-topping");
+  var twoTopping = document.getElementById("two-topping");
+  var threeTopping = document.getElementById("three-topping");
+  var fourTopping = document.getElementById("four-topping");
+  var pizzaPrice = 0;
+  var toppingPrice = 0;
+  var tax = 0;
+  var finalPrice = 0;
 
-  // true
-  if (chosenNumber > -1) {
-    document.getElementById("result").innerHTML =
-      "You chose " + chosenNumber + ", which is a positive number.";
+  //process 
+  if(largePizza.checked == true) {
+    pizzaPrice = 6.00;
   }
-  //  false
-  if (chosenNumber < -1) {
-    document.getElementById("result").innerHTML =
-      "You chose " + chosenNumber + ", which is a negative number.";
+  else if (extraLargePizza.checked == true) {
+    pizzaPrice = 6.00;
   }
+  else {
+   document.getElementById("result").innerHTML =
+     "Please input pizza size"
+  }
+
+  if (oneTopping.checked = true) {
+    toppingPrice = 1.00
+  }
+  else if (twoTopping.checked = true) {
+    toppingPrice = 1.75
+  }
+  else if (threeTopping.checked = true) {
+    toppingPrice = 2.50
+  }
+  else if (fourTopping.checked = true) {
+    toppingPrice = 3.35
+  }
+  else 
+    document.getElementById("result").innerHTML =
+      "Please input number of toppings"
+
+  tax = (toppingPrice + pizzaPrice) * 0.13;
+  finalPrice = toppingPrice + pizzaPrice + tax
+
+  // output
+  document.getElementById("result").innerHTML = 
+    "Your order will cost $" + finalPrice.toFixed(2) ", thank you for ordering!""
 }
